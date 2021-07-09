d4f57d9657f5d5c0f958c06c1549eb742100fa1d
Aidar <khuzin.ja@yandex.ru>
2021-07-09 13:17:35 +0300

    udpate

--- board/models.py
+++ board/models.py
@@ -19,9 +19,9 @@ class Team(models.Model):
     dayNum = models.IntegerField(default=1)
 
     # WIP limits for Analytics, Devops, Testers respectively
-    wip1 = models.IntegerField(name='wip_limit1', default=5)
-    wip2 = models.IntegerField(name='wip_limit2', default=5)
-    wip3 = models.IntegerField(name='wip_limit3', default=5)
+    wip1 = models.IntegerField(name='wip_limit1', default=4)
+    wip2 = models.IntegerField(name='wip_limit2', default=4)
+    wip3 = models.IntegerField(name='wip_limit3', default=4)
 
     def __str__(self):
         return self.name
